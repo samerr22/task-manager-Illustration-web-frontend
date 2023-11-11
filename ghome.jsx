232c71d834f89c229cf5ea3eccf735d46c3b13be
Sameera lakmal <135108578+samerr22@users.noreply.github.com>
2023-11-11 21:32:39 +0530

    Add files via upload

---
+++ ghome.jsx
@@ -0,0 +1,87 @@
+
+import React from 'react';
+import CApture from '/image/Capture.png';
+
+
+
+
+
+
+
+
+
+function ghome(){
+
+
+
+
+ 
+ 
+   
+      
+        
+    return(
+      
+     
+
+   
+   <div>
+   <herader>
+
+    <div style={{display: 'flex', top: '20px', position:'absolute'}}>
+    <div style={{left:'100px', position: 'absolute', fontFamily: 'impact', fontSize:'20px', color:'#7f48a2'}}>deszon.net</div>
+    <div style={{position:'absolute',left: '1000px'}}>
+    <div style={{position:'absolute',left: '100px',fontFamily:'initial', color:'#4d4d4d'}}>Home</div>
+    <div style={{position:'absolute',left: '200px',fontFamily:'initial', color:'#4d4d4d'}}> More</div>
+    <div style={{position:'absolute',left: '300px',fontFamily:'initial', color:'#4d4d4d'}}>About</div>
+    <div style={{position:'absolute',left: '400px',fontFamily:'initial', color:'#4d4d4d'}}>Contact</div>
+    </div>
+    </div>
+
+
+     <div style={{position:'absolute',width:'500px'}}>
+    <div style={{left:'150px', position: 'absolute', top: '130px', fontFamily:'impact',fontSize:'50px', color:'#343434'}}>Task Manager Illustration</div>
+    </div>
+    <div style={{left:'80px', position: 'absolute', width: '500px'}}>
+    <div style={{left:'80px', position: 'absolute', top: '300px',fontFamily:'initial', color:'#cdcdcd'}}>After trying these steps, attempt to run your npm run dev command again. If the issue persists, 
+      there might be a specific configuration or conflict on your system, and it may be helpful to consult 
+      with your IT support or seek assistance on forums related to PowerShell and npm interactions.</div>
+      </div>
+    <button style={{left:'160px', 
+                    position: 'absolute', 
+                    top: '490px', 
+                    border: 'none',
+                    borderRadius:'50px', 
+                    backgroundColor:'#7f48a2',
+                    color:'white',
+                    fontFamily:'initial',
+                    width:'120px',
+                    height:'50px'
+                    }}>READ MORE</button>
+
+    <img src={CApture} alt="user" style={{ width: '700px', height: '600px',position:'absolute', top:'120px',left:'710px' }}/>
+
+   </herader>
+
+   </div>
+
+      
+           
+
+
+
+
+     
+        
+      
+
+
+
+                     
+                   
+
+  
+      
+    )
+    }
+export default ghome;
